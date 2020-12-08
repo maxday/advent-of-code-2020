f2e36de074ef61ca619dc2a1f0f395eafe54ac96
Sebastien Castiel <sebastien@castiel.me>
2020-12-08 16:20:05 -0500

    Day 06

---
+++ day-06-custom-customs/index.js
@@ -0,0 +1,33 @@
+const { testInput, input } = require('./input')
+
+const parseInput = (input) =>
+  input
+    .split('\n\n')
+    .map((s) => s.split('\n'))
+    .map((a) => a.map((e) => [...e]))
+
+const uniq = (arr) => [...new Set([...arr])]
+const sum = (arr) => arr.reduce((acc, e) => acc + e, 0)
+
+const puzzle1 = (input) => sum(parseInput(input).map((arr) => uniq(arr.flat()).length))
+
+console.log(puzzle1(testInput))
+console.log(puzzle1(input))
+
+const intersect = (arr) => {
+  let answers = [...arr[0]]
+  for (let i = 1; i < arr.length; i++) {
+    answers = answers.filter((a) => arr[i].includes(a))
+  }
+  return answers
+}
+
+const puzzle2 = (input) =>
+  sum(
+    parseInput(input)
+      .map(intersect)
+      .map((a) => a.length)
+  )
+
+console.log(puzzle2(testInput))
+console.log(puzzle2(input))
